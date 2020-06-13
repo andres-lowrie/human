585a15f9459b63542b99d313575799b545037619
Andres Lowrie <andres.lowrie@gmail.com>
2020-06-12 21:15:00 -0500

    test(parsers.number): add unit tests

--- src/parsers/README.md
+++ src/parsers/README.md
@@ -1,9 +1,12 @@
 ## Number
 
+Turns numbers into groups of thousands.
+
+Useful for visualizing large numbers
+
 ### logic
 
 ```
-1000 -> 1,000
 1000 -> 1,000
 10000 -> 10,000
 100000 -> 100,000
@@ -12,6 +15,6 @@
 100000000 -> 100,000,000
 1000000000 -> 1,000,000,000
 ```
+
 - has to at least 4 characters
 - while chars are still there, from the back of string, move 3 left, add comma
-
--- src/parsers/number.rs
+++ src/parsers/number.rs
@@ -1,17 +1,18 @@
 extern crate regex;
 
+use super::*;
 use regex::Regex;
 
 #[derive(Debug)]
 pub struct Number {}
 
-impl super::Parser for Number {
+impl Parser for Number {
     fn can_parse_human_into(&self, s: &str) -> bool {
         false
     }
 
     fn can_parse_human_from(&self, s: &str) -> bool {
-        let re = Regex::new(r"^[0-9]").unwrap();
+        let re = Regex::new(r"^[1-9]").unwrap();
         re.is_match(s)
     }
 
@@ -34,5 +35,66 @@ impl super::Parser for Number {
 
     fn do_human_from(&self, s: &str) -> String {
         String::from("something")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_can_parse_human_positive() {
+        let p = Number {};
+        let given = "1000";
+        let want = true;
+        assert_eq!(p.can_parse_human_from(given), want);
+    }
+
+    #[test]
+    fn test_can_parse_human_from_negative() {
+        let p = Number {};
+        let given = "0";
+        let want = false;
+        assert_eq!(p.can_parse_human_from(given), want);
+    }
+
+    #[test]
+    fn test_do_human_to_positive_thou() {
+        let p = Number {};
+        let given = "1000";
+        let want = "1,000";
+        assert_eq!(p.do_human_into(given), want);
+    }
+
+    #[test]
+    fn test_do_human_to_positive_tenthou() {
+        let p = Number {};
+        let given = "10000";
+        let want = "10,000";
+        assert_eq!(p.do_human_into(given), want);
+    }
+
+    #[test]
+    fn test_do_human_to_positive_hunthou() {
+        let p = Number {};
+        let given = "100000";
+        let want = "100,000";
+        assert_eq!(p.do_human_into(given), want);
+    }
+
+    #[test]
+    fn test_do_human_to_positive_mil() {
+        let p = Number {};
+        let given = "1000000";
+        let want = "1,000,000";
+        assert_eq!(p.do_human_into(given), want);
+    }
+
+    #[test]
+    fn test_do_human_to_positive_bil() {
+        let p = Number {};
+        let given = "1000000000";
+        let want = "1,000,000,000";
+        assert_eq!(p.do_human_into(given), want);
     }
 }
